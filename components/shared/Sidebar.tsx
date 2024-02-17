42bbf1bce75c6fb230fc81a51c5580d260a8dbf9
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2024-02-18 00:39:08 +0530

    updated file structure

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { navLinks } from "../../constants";
+import { navLinks } from "../../constants/index";
 import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
 /**
