108085fdadcb61a09f6f0b599d90697fec4aae12
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2024-02-18 17:04:30 +0530

    Successfully resolved issue in app for clerk authentication

--- src/lib/database/models/user.model.ts
+++ src/lib/database/models/user.model.ts
@@ -1,42 +1,41 @@
-import mongoose from "mongoose";
+import { Schema, model, models } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    clerkId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    userName: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-
-    profilePhoto: {
-        type: String,
-        required: true,
-    },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-    },
-
-    planId: {
-        type: Number,
-        default: 1,
-    },
-    creditBalance: {
-        type: Number,
-        default: 20,
-    },
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
 });
-const User = mongoose.models?.User || mongoose.model("User", userSchema);
+
+const User = models?.User || model("User", UserSchema);
+
 export default User;
