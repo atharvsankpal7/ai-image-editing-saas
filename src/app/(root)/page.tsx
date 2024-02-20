e44f3adcd3c2e7942114ec6d30363c8c7ddc0760
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2024-02-20 22:44:04 +0530

    d

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -9,7 +9,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
-  const images = await getAllImages({ page, searchQuery})
+  // const images = await getAllImages({ page, searchQuery})
 
   return (
     <>
@@ -32,7 +32,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
           ))}
         </ul>
       </section>
-
+{/* 
       <section className="sm:mt-12">
         <Collection 
           hasSearch={true}
@@ -40,7 +40,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
           totalPages={images?.totalPage}
           page={page}
         />
-      </section>
+      </section> */}
     </>
   )
 }
