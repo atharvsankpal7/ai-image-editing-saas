7561d0a4bc1e799cc838df71a8b45d0c60d58b37
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2024-02-20 22:38:50 +0530

    this is not going as I thought it would Go

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes: ["/", "/api/webhooks/clerk"],
     ignoredRoutes: ["/no-auth-in-this-route"],
+    apiRoutes: [],
 });
 
 export const config = {
